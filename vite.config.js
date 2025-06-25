bccbdcf6ea86abc4e0f30f9d9fca939600bbb30d
Innocent Bigega <54619123+Innocentenz@users.noreply.github.com>
2025-06-25 21:59:14 +0300

    added the PWA

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,47 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    // VitePWA({
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // ✅ Increase to 5MB (default is 2MB)
-    //   },
-    //   registerType: "autoUpdate",
-    //   includeAssets: ["favicon.ico", "logo.png", "logo.png"],
-    //   manifest: {
-    //     name: "iRembo Finance Management System",
-    //     short_name: "iRembo Finance",
-    //     theme_color: "#000",
-    //     icons: [
-    //       {
-    //         src: "pwa-64x64.png",
-    //         sizes: "64x64",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "pwa-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "pwa-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "any",
-    //       },
-    //       {
-    //         src: "maskable-icon-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //         purpose: "maskable",
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // ✅ Increase to 5MB (default is 2MB)
+      },
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "logo.png", "logo.png"],
+      manifest: {
+        name: "iRembo Finance Management System",
+        short_name: "iRembo Finance",
+        theme_color: "#000",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
